1aea55fe0a0f1f14139f29d0deed7805cbcef3d3
Brett Cimbalik <bcimbali@gmail.com>
2018-09-17 22:10:41 -0500

    keyboard is chromatic and beginning of full key layout

---
+++ src/components/BlackKey/BlackKey.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Key(props) {
+  return(
+    <div
+      className={`key-black key ${props.id === 3 ? 'm-left-3' : props.id === 1 ? 'm-left' : ''}`}
+      onClick={() => props.keyPress(props.note)}
+    >
+
+    </div>
+  );
+};
+
+export default Key;
--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -1,17 +1,31 @@
-import Key from './../Key';
+import BlackKey from './../BlackKey';
 import React from 'react';
-import notes from './../../notes.json';
+import WhiteKey from './../WhiteKey';
+import blackKeys from './../../blackKeys.json';
+import whiteKeys from './../../whiteKeys.json';
 
 function Keyboard(props) {
   return(
     <div className="keyboard">
-      {notes.map(note => (
-        <Key 
-        key={`${note.id}-${note.note}`}
-        keyPress={props.keyPress}
-        note={note.note}
-        />
-      ))}
+      <div className="black-keys">
+        {blackKeys.map(note => (
+          <BlackKey
+          id={note.id}
+          key={`${note.id}-${note.note}`}
+          keyPress={props.keyPress}
+          note={note.note}
+          />
+        ))}
+      </div>
+      <div className="white-keys">
+        {whiteKeys.map(note => (
+          <WhiteKey 
+          key={`${note.id}-${note.note}`}
+          keyPress={props.keyPress}
+          note={note.note}
+          />
+        ))}
+      </div>
     </div>
   );
 };
