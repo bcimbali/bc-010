a397ec2edd3c694627689701801c3c7c504c3ea5
Brett Cimbalik <bcimbali@gmail.com>
2018-10-04 22:04:25 -0500

    destructure control panel

--- src/components/ControlPanel/ControlPanel.jsx
+++ src/components/ControlPanel/ControlPanel.jsx
@@ -4,26 +4,35 @@ import React from 'react';
 import SliderBank from '../SliderBank/SliderBank';
 import oscillatorTypes from './../../oscillatorTypes.json';
 
-function ControlPanel(props) {
+function ControlPanel({
+  attackValue,
+  decayValue,
+  sustainValue,
+  releaseValue,
+  envelopeSliderChange,
+  filterValue,
+  lfoValue,
+  toggleOscillator
+}) {
   return(
     <div className="controlPanel">
     <SliderBank
-      attackValue={props.attackValue}
-      decayValue={props.decayValue}
-      sustainValue={props.sustainValue}
-      releaseValue={props.releaseValue} 
-      envelopeSliderChange={props.envelopeSliderChange} 
+      attackValue={attackValue}
+      decayValue={decayValue}
+      sustainValue={sustainValue}
+      releaseValue={releaseValue} 
+      envelopeSliderChange={envelopeSliderChange} 
     />
     <FilterSliderBank 
-      envelopeSliderChange={props.envelopeSliderChange}
-      filterValue={props.filterValue}
-      lfoValue={props.lfoValue} 
+      envelopeSliderChange={envelopeSliderChange}
+      filterValue={filterValue}
+      lfoValue={lfoValue} 
     />
       {oscillatorTypes.map(oscType => (
         <OscillatorBtn
           abbr={oscType.abbr} 
           key={`${oscType.id}-${oscType.type}`}
-          toggleOscillator={props.toggleOscillator}
+          toggleOscillator={toggleOscillator}
           type={oscType.type}
         />
       ))}
