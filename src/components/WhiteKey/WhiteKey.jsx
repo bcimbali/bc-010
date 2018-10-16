154fb1c6ab604e3c9c072327ae4d784ede650252
Brett Cimbalik <bcimbali@gmail.com>
2018-10-15 22:26:44 -0500

    Add PropTypes to WhiteKey.jsx Prior to this change, PropTypes weren't defined in WhiteKey.jsx.
    This change adds the PropTypes package to the package.json and
    adds propTypes to the WhiteKey.jsx.

--- src/components/WhiteKey/WhiteKey.jsx
+++ src/components/WhiteKey/WhiteKey.jsx
@@ -1,4 +1,14 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+
+Key.propTypes = {
+  displayOctave: PropTypes.number,
+  highlightKey: PropTypes.number,
+  keyCode: PropTypes.number,
+  keyPress: PropTypes.func.isRequired,
+  letter: PropTypes.string,
+  note: PropTypes.string
+};
 
 function Key({highlightKey, keyCode, keyPress, note, letter, displayOctave, startingOctave}) {
   let octaveNote = note + displayOctave;
@@ -16,9 +26,11 @@ function Key({highlightKey, keyCode, keyPress, note, letter, displayOctave, star
           <p>{letter}</p>
         </div>
       </div>
-    
+      {console.log('displayOctave: ', displayOctave)}
     </div>
   );
 };
+
+
 
 export default Key;
