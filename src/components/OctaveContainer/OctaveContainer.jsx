edf97de60cc25da4b1f9a27fe2e46ab8a7aceb70
Brett Cimbalik <bcimbali@gmail.com>
2019-06-29 15:47:56 -0500

    cleans formatting & add propTypes Prior to this change, the new Redux props had no propTypes.
    This change adds in propTypes for decrementOctave and incrementOctave.
    This also, fixes code formatting for OctaveContainer as well.

--- src/components/OctaveContainer/OctaveContainer.jsx
+++ src/components/OctaveContainer/OctaveContainer.jsx
@@ -2,13 +2,15 @@
 import OctaveHeader from "./../OctaveHeader";
 import PropTypes from "prop-types";
 import React from "react";
-import { connect } from 'react-redux';
-import { decrementOctave, incrementOctave } from './actions';
-import { bindActionCreators } from 'redux';
+import { connect } from "react-redux";
+import { decrementOctave, incrementOctave } from "./actions";
+import { bindActionCreators } from "redux";
 
 type Props = {
   adjustOctave: Function,
-  octave: number
+  decrementOctave: Function,
+  incrementOctave: Function,
+  octave: number,
 };
 
 /** Holds the octave display in the control panel. */
@@ -16,7 +18,7 @@ function OctaveContainer({
   adjustOctave,
   decrementOctave,
   incrementOctave,
-  octave
+  octave,
 }: Props) {
   return (
     <section className="octave-container">
@@ -38,16 +40,27 @@ const mapStateToProps = state => ({
   octave: state.octaveContainer.octave,
 });
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({
-  decrementOctave,
-  incrementOctave,
-}, dispatch);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      decrementOctave,
+      incrementOctave,
+    },
+    dispatch,
+  );
 
 OctaveContainer.propTypes = {
   /** Moves ocatve range of keyboard notes up or down */
   adjustOctave: PropTypes.func,
+  /** Lowers octave by 1 */
+  decrementOctave: PropTypes.func,
+  /** Raises octave by 1 */
+  incrementOctave: PropTypes.func,
   /** Current octave for the keyboard. Derived from App.jsx state. */
-  octave: PropTypes.number
+  octave: PropTypes.number,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(OctaveContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(OctaveContainer);
