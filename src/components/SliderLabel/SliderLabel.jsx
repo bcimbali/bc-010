e70fbc9c405f3c08599a352ed71ce5c993286e6e
Brett Cimbalik <bcimbali@gmail.com>
2019-08-27 21:32:11 -0500

    add styled components to envelope sliders

--- src/components/SliderLabel/SliderLabel.jsx
+++ src/components/SliderLabel/SliderLabel.jsx
@@ -1,23 +1,40 @@
 // @flow
 import PropTypes from "prop-types";
 import React from "react";
+import styled from "styled-components";
+
+const Header = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  font-size: 1.5vw;
+  height: 5vh;
+  text-align: center;
+  vertical-align: middle;
+  width: 100%;
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+  text-align: center;
+`;
 
 type Props = {
-  abbr: string
+  abbr: string,
 };
 
 /** Display component just shows the type of slider. */
 function SliderLabel({ abbr }: Props) {
   return (
-    <header className="slider-label-container">
-      <label className="slider-label">{abbr}</label>
-    </header>
+    <Header>
+      <Label>{abbr}</Label>
+    </Header>
   );
 }
 
 SliderLabel.propTypes = {
   /** Abbreviation of the parameter the slider changes (eg. A,D,S,R). */
-  abbr: PropTypes.string
+  abbr: PropTypes.string,
 };
 
 export default SliderLabel;
--- src/components/SliderNumberDisplay/SliderNumberDisplay.jsx
+++ src/components/SliderNumberDisplay/SliderNumberDisplay.jsx
@@ -1,23 +1,40 @@
 // @flow
 import PropTypes from "prop-types";
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  font-size: 1vw;
+  height: 5vh;
+  text-align: center;
+  vertical-align: middle;
+  width: 100%;
+`;
+
+const NumberDisplay = styled.p`
+  margin: 0;
+  text-align: center;
+`;
 
 type Props = {
-  value: number
+  value: number,
 };
 
 /** Display component that shows the real-time value of the slider. */
 function SliderNumberDisplay({ value }: Props) {
   return (
-    <div className="slider-number-container">
-      <p className="slider-number">{value}</p>
-    </div>
+    <Container>
+      <NumberDisplay>{value}</NumberDisplay>
+    </Container>
   );
 }
 
 SliderNumberDisplay.propTypes = {
   /** Value for that slider parameter as it is in App.jsx state. */
-  value: PropTypes.number
+  value: PropTypes.number,
 };
 
 export default SliderNumberDisplay;
